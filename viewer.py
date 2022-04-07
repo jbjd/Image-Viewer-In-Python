46310ce26e168539208ec7db8410cccdbea929a6
James Demetris <63110925+jbjd@users.noreply.github.com>
2022-04-06 21:18:33 -0400

    Visual update and trash button

--- viewer.py
+++ viewer.py
@@ -1,10 +1,18 @@
+#  example compile code: python -m nuitka --windows-disable-console viewer.py  (pyinstaller is very slow, please use nuitka if you plan to compile it yourself)
+from operator import index
 from sys import argv
 from tkinter import Tk, Canvas
 from PIL import Image, ImageTk, ImageDraw
-import os.path
+from os import path
+from send2trash import send2trash
 from pathlib import Path
+from ctypes import windll
+windll.shcore.SetProcessDpiAwareness(1)
 debug = False
-ICONSPACE = 30
+SPACE = 32
+LINECOL = (170, 170, 170)
+ICONCOL = (100, 104, 102)
+ICONHOV = (95, 92, 88)
 
 #  fits width and height to tkinter window
 def dimensionFinder(w, h):
@@ -26,7 +34,7 @@ def imageLoader(path, drawtop):
     global app
     global exitb
     global minib
-    global hoveredExit
+    global trashb
 
     if path not in cache:
         image = Image.open(path)
@@ -46,16 +54,31 @@ def imageLoader(path, drawtop):
         canvas.create_image(data[1], data[2], image=data[0], anchor='nw')
     if drawtop:
         canvas.create_image(0, 0, image=topbar, anchor='nw')
-        b = canvas.create_image(app.winfo_width()-ICONSPACE, 0, image=exitb, anchor='nw', tag='exiter')
-        b2 = canvas.create_image(app.winfo_width()-ICONSPACE-ICONSPACE, 0, image=minib, anchor='nw', tag='minimizer')
+        canvas.create_text(36, 5, text=path.name, fill="white", anchor='nw', font=('cambria 11'))
+        b = canvas.create_image(app.winfo_width()-SPACE, 0, image=exitb, anchor='nw', tag='exiter')
+        b2 = canvas.create_image(app.winfo_width()-SPACE-SPACE, 0, image=minib, anchor='nw', tag='minimizer')
+        t = canvas.create_image(0, 0, image=trashb, anchor='nw', tag='trasher')
         canvas.tag_bind(b, '<Button-1>', _exit)
-        canvas.tag_bind(b2, '<Button-1>', _minimize)
+        canvas.tag_bind(b2,'<Button-1>', _minimize)
+        canvas.tag_bind(t, '<Button-1>', _trashWindow)
         canvas.tag_bind(b, '<Enter>', _hoverExit)
         canvas.tag_bind(b, '<Leave>', _removeHoverExit)
-        canvas.tag_bind(b2, '<Enter>', _hoverMini)
-        canvas.tag_bind(b2, '<Leave>', _removeHoverMini)
+        canvas.tag_bind(b2,'<Enter>', _hoverMini)
+        canvas.tag_bind(b2,'<Leave>', _removeHoverMini)
+        canvas.tag_bind(t, '<Enter>', _hoverTrash)
+        canvas.tag_bind(t, '<Leave>', _removeHoverTrash)
 
 # series of functions to change icons on hover
+def _removeHoverTrash(event):
+    global canvas
+    global trashb
+    canvas.itemconfig('trasher', image=trashb)
+
+def _hoverTrash(event):
+    global canvas
+    global hoverTrash
+    canvas.itemconfig('trasher', image=hoverTrash)
+
 def _removeHoverMini(event):
     global canvas
     global minib
@@ -84,18 +107,18 @@ def _minimize(event):
 # closes app    
 def _exit(event):
     global app
-    app.destroy()
+    app.quit()
 
 # move between images when mouse scolls
-def _on_mousewheel(event):
+def _mouseScroll(event):
     global curInd
     global files
-    global SECONDTOLAST
+    global secondToLast
     global drawtop
     if event.delta > 0 and curInd > 0:
         curInd -= 1
         imageLoader(files[curInd], drawtop)
-    elif event.delta < 0 and curInd < SECONDTOLAST:
+    elif event.delta < 0 and curInd < secondToLast:
         curInd += 1
         imageLoader(files[curInd], drawtop)
 
@@ -103,6 +126,19 @@ def _on_mousewheel(event):
 def _resize(event):
     global cache
     cache = dict()
+
+# ask user if they want to delete image
+def _trashWindow(event):
+    global curInd
+    global files
+    global drawtop
+    global secondToLast
+    send2trash(files[curInd])
+    del files[curInd]
+    secondToLast = len(files)-1
+    if curInd >= len(files):
+        curInd = len(files)-1
+    imageLoader(files[curInd], drawtop)
 
 # skip clicks to menu, draws menu if not present
 def _click(event):
@@ -122,7 +158,7 @@ def drawWrapper(event):
     imageLoader(files[curInd], drawtop)
 
 if len(argv) > 1 or debug:
-    image = Path(r"C:\path\for\testing") if debug else Path(argv[1])
+    image = Path(r"C:\somewhere\example.png") if debug else Path(argv[1])
     # initialize main window + important data
     drawtop = False  # bool if menu at top is drawn
     cache = dict()  # cache calculated data on images
@@ -135,28 +171,48 @@ if len(argv) > 1 or debug:
     app.update()  # updates winfo width and height to the current size, this is necessary
 
     # make assests for menu
-    topbar = ImageTk.PhotoImage(Image.new('RGBA', (app.winfo_width(), ICONSPACE), (70, 70, 70, 160)))
-    exitb = ImageTk.PhotoImage(Image.new('RGBA', (ICONSPACE, ICONSPACE), (190, 50, 50, 180)))
-    hoveredExit = ImageTk.PhotoImage(Image.new('RGBA', (ICONSPACE, ICONSPACE), (190, 30, 30, 180)))
-    minib = Image.new('RGBA', (ICONSPACE, ICONSPACE), (120, 120, 120, 160))
+    topbar = ImageTk.PhotoImage(Image.new('RGBA', (app.winfo_width(), SPACE), (70, 70, 70, 160)))
+    exitb = ImageTk.PhotoImage(Image.new('RGBA', (SPACE, SPACE), (190, 40, 40)))
+    hoveredExit = Image.new('RGBA', (SPACE, SPACE), (180, 25, 20))
+    draw = ImageDraw.Draw(hoveredExit) 
+    draw.line((6, 6, 26, 26), width=2, fill=LINECOL)
+    draw.line((6, 26, 26, 6), width=2, fill=LINECOL)
+    hoveredExit = ImageTk.PhotoImage(draw._image)
+    minib = Image.new('RGBA', (SPACE, SPACE), ICONCOL)
     draw = ImageDraw.Draw(minib) 
-    draw.line((6, 24, 22, 24), width=2, fill=(170, 170, 170, 160))
+    draw.line((6, 24, 24, 24), width=2, fill=LINECOL)
     minib = ImageTk.PhotoImage(draw._image)
-    hoveredMini = Image.new('RGBA', (ICONSPACE, ICONSPACE), (110, 110, 110, 160))
+    hoveredMini = Image.new('RGBA', (SPACE, SPACE), ICONHOV)
     draw = ImageDraw.Draw(hoveredMini) 
-    draw.line((6, 24, 22, 24), width=2, fill=(180, 180, 180, 160))
+    draw.line((6, 24, 24, 24), width=2, fill=LINECOL)
     hoveredMini = ImageTk.PhotoImage(draw._image)
+    trashb = Image.new('RGBA', (SPACE, SPACE), ICONCOL)
+    draw = ImageDraw.Draw(trashb) 
+    draw.line((9, 9, 9, 22), width=2, fill=LINECOL)
+    draw.line((21, 9, 21, 22), width=2, fill=LINECOL)
+    draw.line((9, 22, 21, 22), width=2, fill=LINECOL)
+    draw.line((7, 9, 24, 9), width=2, fill=LINECOL)
+    draw.line((12, 8, 19, 8), width=3, fill=LINECOL)
+    trashb = ImageTk.PhotoImage(draw._image)
+    hoverTrash = Image.new('RGBA', (SPACE, SPACE), ICONHOV)
+    draw = ImageDraw.Draw(hoverTrash) 
+    draw.line((9, 9, 9, 22), width=2, fill=LINECOL)
+    draw.line((21, 9, 21, 22), width=2, fill=LINECOL)
+    draw.line((9, 22, 21, 22), width=2, fill=LINECOL)
+    draw.line((7, 9, 24, 9), width=2, fill=LINECOL)
+    draw.line((12, 8, 19, 8), width=3, fill=LINECOL)
+    hoverTrash = ImageTk.PhotoImage(draw._image)
 
     # events based on input
-    app.bind("<MouseWheel>", _on_mousewheel)
+    app.bind("<MouseWheel>", _mouseScroll)
     canvas.bind("<Button-1>", _click)
     app.bind("<Configure>", _resize)
     app.bind("<FocusIn>", drawWrapper)
 
-    dir = os.path.dirname(image)+'\\'
+    dir = path.dirname(image)+'/'
     files = list(p for p in Path(dir).glob("*") if p.suffix in {".png", ".jpg", ".jpeg", ".webp"})
 
-    SECONDTOLAST = len(files)-1
+    secondToLast = len(files)-1
     files.sort()
     curInd = files.index(image)
     
