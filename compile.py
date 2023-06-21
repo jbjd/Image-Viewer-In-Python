5cbf28eae7b0cf10725e001972c821dc65015313
demetri7 <demetri7@msu.edu>
2023-06-21 19:08:50 -0400

    dynamic compile file

--- compile.py
+++ compile.py
@@ -1,15 +1,23 @@
 import subprocess
 import os
+from shutil import rmtree
 
-cmd_str = 'python -m nuitka --windows-disable-console --windows-icon-from-ico="C:\PythonCode\Viewer\icon\icon.ico" --mingw64 viewer.py'
-process = subprocess.Popen(cmd_str, shell=True)
+WORKING_DIR = 'C:/PythonCode/Viewer/'
 
+cmd_str = f'python -m nuitka --windows-disable-console --windows-icon-from-ico="{WORKING_DIR}icon/icon.ico" --mingw64 {WORKING_DIR}viewer.py'
+process = subprocess.Popen(cmd_str, shell=True, cwd=WORKING_DIR)
 
-if(os.path.isfile('C:/Program Files/Personal Image Viewer/viewer2.exe')):
-	os.remove('C:/Program Files/Personal Image Viewer/viewer2.exe')
+try:
+	if(os.path.isfile('C:/Program Files/Personal Image Viewer/viewer2.exe')):
+		os.remove('C:/Program Files/Personal Image Viewer/viewer2.exe')
 
-if(os.path.isfile('C:/Program Files/Personal Image Viewer/viewer.exe')):
-	os.rename('C:/Program Files/Personal Image Viewer/viewer.exe', 'C:/Program Files/Personal Image Viewer/viewer2.exe')
+	if(os.path.isfile('C:/Program Files/Personal Image Viewer/viewer.exe')):
+		os.rename('C:/Program Files/Personal Image Viewer/viewer.exe', 'C:/Program Files/Personal Image Viewer/viewer2.exe')
 
-process.wait()
-os.rename('C:/PythonCode/Viewer/viewer.exe', 'C:/Program Files/Personal Image Viewer/viewer.exe')
+	process.wait()
+	rmtree(WORKING_DIR+'viewer.build')
+	os.remove(WORKING_DIR+'viewer.cmd')
+	os.rename(WORKING_DIR+'viewer.exe', 'C:/Program Files/Personal Image Viewer/viewer.exe')
+except Exception as e:
+	print(e)
+	print("No root privileges")
