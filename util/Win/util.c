ab20fccfa9ea58e1efa0c57dda047db4b51b12b0
demetri7 <demetri7@msu.edu>
2023-07-02 17:00:06 -0400

    fix error in str comp fuction

--- util/Win/util.c
+++ util/Win/util.c
@@ -1,3 +1,5 @@
+// gcc util.c -c
+// gcc -shared -o util.dll util.o
 #include <wchar.h>
 
 int myCmpW(const wchar_t* a, const wchar_t* b){
@@ -13,22 +15,22 @@ int myCmpW(const wchar_t* a, const wchar_t* b){
 	unsigned long long al = wcslen(a), bl = wcslen(b), ai = 0, bi = 0, aval, bval;
 
 	while(ai < al && bi < bl){
-		if(a[ai] != b[bi]){
-			if(a[ai] >= '0' && a[ai] <= '9' && b[bi] >= '0' && b[bi] <= '9'){
-				aval = bval = 0;
-				while(a[ai] >= '0' && a[ai] <= '9' && ai < al){
-					aval = (10*aval) + (a[ai] - '0');
-					++ai;
-				}
-				while(b[bi] >= '0' && b[bi] <= '9' && bi < bl){
-					bval = (10*bval) + (b[bi] - '0');
-					++bi;
-				}
-				if(aval != bval){
-					return aval > bval ? 1 : -1;
-				}
-				continue;
+		if(iswdigit(a[ai]) && iswdigit(b[bi])){
+			aval = bval = 0;
+			while(iswdigit(a[ai]) && ai < al){
+				aval = (10*aval) + (a[ai] - L'0');
+				++ai;
 			}
+			while(iswdigit(b[bi]) && bi < bl){
+				bval = (10*bval) + (b[bi] - L'0');
+				++bi;
+			}
+			if(aval != bval){
+				return aval > bval ? 1 : -1;
+			}
+			continue;
+		}
+		if(a[ai] != b[bi]){
 			return a[ai] > b[bi] ? 1 : -1;
 		}
 		++ai;
