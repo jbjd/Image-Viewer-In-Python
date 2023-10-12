c74629351b84fb1541515e2c3a2bbda427ce2e3e
demetri7 <demetri7@msu.edu>
2023-10-12 15:36:34 -0500

    better importing for icon factory

--- factories/icon_factory.py
+++ factories/icon_factory.py
@@ -1,4 +1,5 @@
-from PIL import Image, ImageDraw
+from PIL.Image import Image, new
+from PIL.ImageDraw import ImageDraw, Draw
 from PIL.ImageTk import PhotoImage
 
 
@@ -19,35 +20,29 @@ class IconFactory:
         self.icon_size: int = icon_size
         self.screen_width: int = screen_width
 
-        self.icon_default = Image.new("RGB", (icon_size, icon_size), self.ICON_RGB)
-        self.icon_hovered_default = Image.new(
+        self.icon_default: Image = new("RGB", (icon_size, icon_size), self.ICON_RGB)
+        self.icon_hovered_default: Image = new(
             "RGB", (icon_size, icon_size), self.ICON_HOVERED_RGB
         )
 
     def make_topbar(self) -> PhotoImage:
         return PhotoImage(
-            Image.new("RGBA", (self.screen_width, self.icon_size), self.TOPBAR_RGBA)
+            new("RGBA", (self.screen_width, self.icon_size), self.TOPBAR_RGBA)
         )
 
     def make_exit_icon(self) -> PhotoImage:
-        return PhotoImage(
-            Image.new("RGB", (self.icon_size, self.icon_size), self.EXIT_RGB)
-        )
+        return PhotoImage(new("RGB", (self.icon_size, self.icon_size), self.EXIT_RGB))
 
     def make_exit_icon_hovered(self) -> PhotoImage:
-        draw = ImageDraw.Draw(
-            Image.new("RGB", (self.icon_size, self.icon_size), self.EXIT_HOVER_RGB)
-        )
+        draw = Draw(new("RGB", (self.icon_size, self.icon_size), self.EXIT_HOVER_RGB))
         draw.line((6, 6, 26, 26), width=2, fill=self.LINE_RGB)
         draw.line((6, 26, 26, 6), width=2, fill=self.LINE_RGB)
         return PhotoImage(draw._image)
 
-    def _make_icon_base(self) -> tuple[ImageDraw.ImageDraw, ImageDraw.ImageDraw]:
-        return ImageDraw.Draw(self.icon_default.copy()), ImageDraw.Draw(
-            self.icon_hovered_default.copy()
-        )
+    def _make_icon_base(self) -> tuple[ImageDraw, ImageDraw]:
+        return Draw(self.icon_default.copy()), Draw(self.icon_hovered_default.copy())
 
-    def _draw_minify_symbol(self, draw: ImageDraw.ImageDraw) -> PhotoImage:
+    def _draw_minify_symbol(self, draw: ImageDraw) -> PhotoImage:
         draw.line((6, 24, 24, 24), width=2, fill=self.LINE_RGB)
         return PhotoImage(draw._image)
 
@@ -55,7 +50,7 @@ class IconFactory:
         draw, draw_hovered = self._make_icon_base()
         return self._draw_minify_symbol(draw), self._draw_minify_symbol(draw_hovered)
 
-    def _draw_trash_symbol(self, draw: ImageDraw.ImageDraw) -> PhotoImage:
+    def _draw_trash_symbol(self, draw: ImageDraw) -> PhotoImage:
         draw.line((9, 9, 9, 22), width=2, fill=self.LINE_RGB)
         draw.line((21, 9, 21, 22), width=2, fill=self.LINE_RGB)
         draw.line((9, 22, 21, 22), width=2, fill=self.LINE_RGB)
@@ -67,7 +62,7 @@ class IconFactory:
         draw, draw_hovered = self._make_icon_base()
         return self._draw_trash_symbol(draw), self._draw_trash_symbol(draw_hovered)
 
-    def _draw_down_arrow(self, draw: ImageDraw.ImageDraw) -> PhotoImage:
+    def _draw_down_arrow(self, draw: ImageDraw) -> PhotoImage:
         draw.line((6, 11, 16, 21), width=2, fill=self.LINE_RGB)
         draw.line((16, 21, 26, 11), width=2, fill=self.LINE_RGB)
         return PhotoImage(draw._image)
@@ -78,7 +73,7 @@ class IconFactory:
         draw, draw_hovered = self._make_icon_base()
         return self._draw_down_arrow(draw), self._draw_down_arrow(draw_hovered)
 
-    def _draw_up_arrow(self, draw: ImageDraw.ImageDraw) -> PhotoImage:
+    def _draw_up_arrow(self, draw: ImageDraw) -> PhotoImage:
         draw.line((6, 21, 16, 11), width=2, fill=self.LINE_RGB)
         draw.line((16, 11, 26, 21), width=2, fill=self.LINE_RGB)
         draw.line((16, 11, 16, 11), width=1, fill=self.LINE_RGB)
@@ -90,17 +85,15 @@ class IconFactory:
         draw, draw_hovered = self._make_icon_base()
         return self._draw_up_arrow(draw), self._draw_up_arrow(draw_hovered)
 
-    def _draw_rename_symbol(self, draw: ImageDraw.ImageDraw) -> PhotoImage:
+    def _draw_rename_symbol(self, draw: ImageDraw) -> PhotoImage:
         draw.rectangle((7, 10, 25, 22), width=1, outline=self.LINE_RGB)
         draw.line((7, 16, 16, 16), width=3, fill=self.LINE_RGB)
         draw.line((16, 8, 16, 24), width=2, fill=self.LINE_RGB)
         return PhotoImage(draw._image)
 
     def make_rename_icons(self) -> tuple[PhotoImage, PhotoImage]:
-        icon_default_alpha = Image.new(
-            "RGBA", (self.icon_size, self.icon_size), (0, 0, 0, 0)
-        )
-        draw, draw_hovered = ImageDraw.Draw(icon_default_alpha.copy()), ImageDraw.Draw(
+        icon_default_alpha = new("RGBA", (self.icon_size, self.icon_size), (0, 0, 0, 0))
+        draw, draw_hovered = Draw(icon_default_alpha.copy()), Draw(
             icon_default_alpha.copy()
         )
         draw_hovered.rectangle((4, 5, 28, 27), width=1, fill=self.ICON_HOVERED_RGB)
