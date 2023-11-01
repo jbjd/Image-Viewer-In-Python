6a5be3830511657b4ef8fada74dd17da8f73f0d1
demetri7 <demetri7@msu.edu>
2023-10-31 19:19:08 -0500

    print can't be seen due to windows-disable-console flag

--- main.py
+++ main.py
@@ -6,9 +6,3 @@ from viewer import Viewer
 if len(argv) > 1:
     path_to_exe: str = os.path.dirname(os.path.realpath(argv[0]))
     Viewer(argv[1], path_to_exe)
-else:
-    print(
-        "An Image Viewer written in Python\n"
-        "Run with 'python -m viewer C:/path/to/an/image'"
-        ' or convert to an exe and select "open with" on your image'
-    )
