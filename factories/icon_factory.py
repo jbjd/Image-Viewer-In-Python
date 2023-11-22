3492417d6662a150b1c019fb342907becff56896
demetri7 <demetri7@msu.edu>
2023-11-21 23:16:04 -0600

    fix type checking in factory

--- factories/icon_factory.py
+++ factories/icon_factory.py
@@ -6,10 +6,10 @@ from PIL.ImageTk import PhotoImage
 # Currently draws symbols at set points, not relative to size of icons
 # Monitors other than mine might get wrong size symbols
 class IconFactory:
-    LINE_RGB: tuple = (170, 170, 170)
-    ICON_RGB: tuple = (100, 104, 102)
-    ICON_HOVERED_RGB: tuple = (95, 92, 88)
-    TOPBAR_RGBA: tuple = (60, 60, 60, 170)
+    LINE_RGB: tuple[int, int, int] = (170, 170, 170)
+    ICON_RGB: tuple[int, int, int] = (100, 104, 102)
+    ICON_HOVERED_RGB: tuple[int, int, int] = (95, 92, 88)
+    TOPBAR_RGBA: tuple[int, int, int, int] = (60, 60, 60, 170)
 
     __slots__ = ("icon_size", "screen_width", "icon_default", "icon_hovered_default")
 
@@ -36,14 +36,14 @@ class IconFactory:
         draw = Draw(new("RGB", (self.icon_size, self.icon_size), EXIT_HOVER_RGB))
         draw.line((6, 6, 26, 26), width=2, fill=self.LINE_RGB)
         draw.line((6, 26, 26, 6), width=2, fill=self.LINE_RGB)
-        return PhotoImage(draw._image)
+        return PhotoImage(draw._image)  # type: ignore
 
     def _make_icon_base(self) -> tuple[ImageDraw, ImageDraw]:
         return Draw(self.icon_default.copy()), Draw(self.icon_hovered_default.copy())
 
     def _draw_minify_symbol(self, draw: ImageDraw) -> PhotoImage:
         draw.line((6, 24, 24, 24), width=2, fill=self.LINE_RGB)
-        return PhotoImage(draw._image)
+        return PhotoImage(draw._image)  # type: ignore
 
     def make_minify_icons(self) -> tuple[PhotoImage, PhotoImage]:
         draw, draw_hovered = self._make_icon_base()
@@ -55,7 +55,7 @@ class IconFactory:
         draw.line((9, 22, 21, 22), width=2, fill=self.LINE_RGB)
         draw.line((7, 9, 24, 9), width=2, fill=self.LINE_RGB)
         draw.line((12, 8, 19, 8), width=3, fill=self.LINE_RGB)
-        return PhotoImage(draw._image)
+        return PhotoImage(draw._image)  # type: ignore
 
     def make_trash_icons(self) -> tuple[PhotoImage, PhotoImage]:
         draw, draw_hovered = self._make_icon_base()
@@ -64,7 +64,7 @@ class IconFactory:
     def _draw_down_arrow(self, draw: ImageDraw) -> PhotoImage:
         draw.line((6, 11, 16, 21), width=2, fill=self.LINE_RGB)
         draw.line((16, 21, 26, 11), width=2, fill=self.LINE_RGB)
-        return PhotoImage(draw._image)
+        return PhotoImage(draw._image)  # type: ignore
 
     def make_dropdown_hidden_icons(
         self,
@@ -76,7 +76,7 @@ class IconFactory:
         draw.line((6, 21, 16, 11), width=2, fill=self.LINE_RGB)
         draw.line((16, 11, 26, 21), width=2, fill=self.LINE_RGB)
         draw.line((16, 11, 16, 11), width=1, fill=self.LINE_RGB)
-        return PhotoImage(draw._image)
+        return PhotoImage(draw._image)  # type: ignore
 
     def make_dropdown_showing_icons(
         self,
@@ -88,7 +88,7 @@ class IconFactory:
         draw.rectangle((7, 10, 25, 22), width=1, outline=self.LINE_RGB)
         draw.line((7, 16, 16, 16), width=3, fill=self.LINE_RGB)
         draw.line((16, 8, 16, 24), width=2, fill=self.LINE_RGB)
-        return PhotoImage(draw._image)
+        return PhotoImage(draw._image)  # type: ignore
 
     def make_rename_icons(self) -> tuple[PhotoImage, PhotoImage]:
         icon_default_alpha = new("RGBA", (self.icon_size, self.icon_size), (0, 0, 0, 0))
