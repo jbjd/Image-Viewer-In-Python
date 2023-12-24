c990474ace64becc4f933d7cb4e398eebe084fa7
demetri7 <demetri7@msu.edu>
2023-12-24 16:13:00 -0600

    compile fail will not mess up old install

--- compile.py
+++ compile.py
@@ -20,12 +20,10 @@ else:
 if not is_admin:
     raise Exception("compile.py needs admin privileges to run")
 
-INSTALL_PATH: str = "C:/Program Files/Personal Image Viewer/"
-
 WORKING_DIR: str = f"{os.path.dirname(os.path.realpath(__file__))}/"
-
-if not WORKING_DIR:
-    raise Exception("Failed to find this file's directory")
+INSTALL_PATH: str = "C:/Program Files/Personal Image Viewer/"
+TEMP_PATH: str = f"{WORKING_DIR}/TEMP/"  # set up here, then copy to install path
+DATA_FILE_PATHS: list[str] = ["icon/icon.ico", "dll/libturbojpeg.dll"]
 
 
 print("Starting up nuitka")
@@ -36,26 +34,37 @@ cmd_str = f'python -m nuitka --windows-disable-console \
     --follow-import-to="managers"  --follow-import-to="helpers" {WORKING_DIR}main.py'
 process = subprocess.Popen(cmd_str, shell=True, cwd=WORKING_DIR)
 
-exe_install_path = f"{INSTALL_PATH}viewer.exe"
-old_exe_install_path = f"{INSTALL_PATH}viewer2.exe"
-data_file_paths: list[str] = ["icon/icon.ico", "dll/libturbojpeg.dll"]
+
+def cleanup_after_compile() -> None:
+    shutil.rmtree(f"{WORKING_DIR}main.build/", ignore_errors=True)
+    try:
+        os.remove(f"{WORKING_DIR}main.cmd")
+    except FileNotFoundError:
+        pass
+
 
 try:
-    # keep a copy of previously installed version just in case
-    if os.path.isfile(exe_install_path):
-        shutil.move(exe_install_path, old_exe_install_path)
+    os.makedirs(INSTALL_PATH, exist_ok=True)
 
-    for data_file_path in data_file_paths:
-        shutil.copy(f"{WORKING_DIR}{data_file_path}", f"{INSTALL_PATH}{data_file_path}")
-
-    print("Waiting for nuitka compilation")
-    process.wait()
-    os.remove(f"{WORKING_DIR}main.cmd")
-    os.rename(f"{WORKING_DIR}main.exe", exe_install_path)
-    shutil.rmtree(f"{WORKING_DIR}main.build")
-    print(f"\nComplete, installed to {INSTALL_PATH}")
-finally:
-    # move backup exe to original exe
-    shutil.move(old_exe_install_path, exe_install_path)
-    # and stop the new compile thread
+    for data_file_path in DATA_FILE_PATHS:
+        old_path: str = f"{WORKING_DIR}{data_file_path}"
+        new_path: str = f"{TEMP_PATH}{data_file_path}"
+        os.makedirs(os.path.dirname(new_path), exist_ok=True)
+        shutil.copy(old_path, new_path)
+except Exception as e:
+    # stop the compile thread
     process.kill()
+    cleanup_after_compile()
+    raise e
+
+print("Waiting for nuitka compilation")
+process.wait()
+
+os.rename(f"{WORKING_DIR}main.exe", f"{TEMP_PATH}viewer.exe")
+
+# copy temp path to install path
+shutil.rmtree(INSTALL_PATH, ignore_errors=True)
+os.rename(TEMP_PATH, INSTALL_PATH)
+
+cleanup_after_compile()
+print(f"\nFinished, installed to {INSTALL_PATH}")
