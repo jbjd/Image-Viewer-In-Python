6e5f175803aa3f25020afdfdccab91b4cd1e5961
jbjd <jimdemetris@yahoo.com>
2024-01-07 20:18:28 -0600

    --standalone flag has entered the building

--- compile.py
+++ compile.py
@@ -2,7 +2,7 @@ import ctypes
 import os
 import shutil
 import subprocess
-from argparse import ArgumentParser, REMAINDER
+from argparse import REMAINDER, ArgumentParser
 
 try:
     import nuitka  # noqa: F401
@@ -12,7 +12,8 @@ except ImportError:
     )
 
 WORKING_DIR: str = f"{os.path.dirname(os.path.realpath(__file__))}/"
-TEMP_PATH: str = f"{WORKING_DIR}TEMP/"  # setup here, then copy to install path
+TEMP_PATH: str = f"{WORKING_DIR}main.dist/"  # setup here, then copy to install path
+VALID_NUITKA_ARGS = {"--mingw64", "--clang", "--standalone"}
 INSTALL_PATH: str
 EXECUTABLE_EXT: str
 DATA_FILE_PATHS: list[str]
@@ -25,8 +26,6 @@ else:
     INSTALL_PATH = "/usr/local/personal-image-viewer/"
     EXECUTABLE_EXT = ".bin"
     DATA_FILE_PATHS = ["icon/icon.png"]
-
-VALID_NUITKA_ARGS = {"--mingw64", "--clang"}
 
 parser = ArgumentParser(
     description="Compiles Personal Image Viewer to an executable, must be run as root",
@@ -42,12 +41,14 @@ for extra_arg in extra_args_list:
     if extra_arg not in VALID_NUITKA_ARGS:
         raise ValueError(f"Unkown arguement {extra_arg}")
 
-# TODO: if --standalone, I should add no follows to things like Qt/PDF/tests/etc
+extra_args: str = " ".join(extra_args_list).strip()
 
-extra_args: str = " ".join(extra_args_list)
-
-if args.install_path:
-    INSTALL_PATH = args.install_path
+if "--standalone" in extra_args_list:
+    extra_args += " --enable-plugin=tk-inter"
+    with open(os.path.join(WORKING_DIR, "skippable_imports.txt")) as fp:
+        extra_args += " --nofollow-import-to=" + " --nofollow-import-to=".join(
+            fp.read().strip().split("\n")
+        )
 
 is_root: bool
 if os.name == "nt":
@@ -77,52 +78,46 @@ if args.python_path is None:
     else:
         args.python_path = "python3"
 
-# begin nuitka compilation in another thread
+# begin nuitka compilation in subprocess
 print("Starting compilation with nuitka")
 print("Using python install found at", args.python_path)
 cmd_str = f'{args.python_path} -m nuitka --windows-disable-console \
     --windows-icon-from-ico="{WORKING_DIR}image_viewer/icon/icon.ico" \
     --follow-import-to="factories" --follow-import-to="util" \
     --follow-import-to="viewer" --follow-import-to="managers"  \
-    --follow-import-to="helpers" "{WORKING_DIR}image_viewer/main.py"'
-
-cmd_str += f" {extra_args}"
+    --follow-import-to="helpers" {extra_args} "{WORKING_DIR}image_viewer/main.py"'
 
 process = subprocess.Popen(cmd_str, shell=True, cwd=WORKING_DIR)
 
 
 def cleanup_after_compile() -> None:
     shutil.rmtree(f"{WORKING_DIR}main.build/", ignore_errors=True)
+    shutil.rmtree(f"{WORKING_DIR}main.dist/", ignore_errors=True)
     try:
         os.remove(f"{WORKING_DIR}main.cmd")
     except FileNotFoundError:
         pass
 
 
-# setup data files
-try:
-    os.makedirs(INSTALL_PATH, exist_ok=True)
-
-    for data_file_path in DATA_FILE_PATHS:
-        old_path: str = f"{WORKING_DIR}image_viewer/{data_file_path}"
-        new_path: str = f"{TEMP_PATH}{data_file_path}"
-        os.makedirs(os.path.dirname(new_path), exist_ok=True)
-        shutil.copy(old_path, new_path)
-except Exception as e:
-    # stop the compile thread
-    process.kill()
-    cleanup_after_compile()
-    raise e
+if args.install_path:
+    INSTALL_PATH = args.install_path
+os.makedirs(INSTALL_PATH, exist_ok=True)
 
 print("Waiting for nuitka compilation...")
 process.wait()
 
-os.rename(f"{WORKING_DIR}main{EXECUTABLE_EXT}", f"{TEMP_PATH}viewer{EXECUTABLE_EXT}")
+try:
+    for data_file_path in DATA_FILE_PATHS:
+        old_path = f"{WORKING_DIR}image_viewer/{data_file_path}"
+        new_path = f"{TEMP_PATH}{data_file_path}"
+        os.makedirs(os.path.dirname(new_path), exist_ok=True)
+        shutil.copy(old_path, new_path)
 
-# copy temp path to install path
-shutil.rmtree(INSTALL_PATH, ignore_errors=True)
-os.rename(TEMP_PATH, INSTALL_PATH)
+    os.rename(f"{TEMP_PATH}main{EXECUTABLE_EXT}", f"{TEMP_PATH}viewer{EXECUTABLE_EXT}")
+    shutil.rmtree(INSTALL_PATH, ignore_errors=True)
+    os.rename(TEMP_PATH, INSTALL_PATH)
+finally:
+    cleanup_after_compile()
 
-cleanup_after_compile()
 print("\nFinished")
 print("Installed to", INSTALL_PATH)
