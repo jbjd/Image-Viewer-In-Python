8d56b7a26aeaca0472845650538529060d65efe5
jbjd <jimdemetris@yahoo.com>
2024-01-21 20:18:37 -0600

    some testing updates

--- tests/test_os_util.py
+++ tests/test_os_util.py
@@ -1,4 +1,6 @@
-from image_viewer.util.os import clean_str_for_OS_path
+import os
+
+from image_viewer.util.os import clean_str_for_OS_path, walk_dir
 
 
 def test_clean_str_for_OS_path():
@@ -6,3 +8,11 @@ def test_clean_str_for_OS_path():
     bad_name: str = "String/With/Illegal/Characters.png"
     good_name: str = clean_str_for_OS_path(bad_name)
     assert good_name == "StringWithIllegalCharacters.png"
+
+
+def test_walk_dir():
+    """Test that walk_dir behaves like expected"""
+    files = [
+        p for p in walk_dir(os.path.join(os.path.dirname(__file__), "example_images"))
+    ]
+    assert len(files) == 5
