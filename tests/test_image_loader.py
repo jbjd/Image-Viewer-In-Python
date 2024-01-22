4d9d5f7a9fc19f02bd258f8bd93069d2ed3b3c00
jbjd <jimdemetris@yahoo.com>
2024-01-21 23:14:22 -0600

    add typehint

--- tests/test_image_loader.py
+++ tests/test_image_loader.py
@@ -6,7 +6,7 @@ from image_viewer.helpers.image_resizer import ImageResizer
 
 
 @pytest.fixture
-def mock_image_loader(image_resizer: ImageResizer):
+def image_loader(image_resizer: ImageResizer) -> ImageLoader:
     image_loader = ImageLoader(None, image_resizer, lambda *_: None)  # type: ignore
     # file_pointer will (and must) exist before calling reset
     # since first thing done is loading an image where its normally set
@@ -14,27 +14,27 @@ def mock_image_loader(image_resizer: ImageResizer):
     return image_loader
 
 
-def test_next_frame(mock_image_loader):
+def test_next_frame(image_loader: ImageLoader):
     """Test expected behavior from getting next frame and reseting"""
 
     # normally has tuples with (PhotoImage, int) but just (int, int) for testing
-    mock_image_loader.aniamtion_frames = [(1, 1), (2, 2), (3, 3)]
+    image_loader.aniamtion_frames = [(1, 1), (2, 2), (3, 3)]
 
-    example_frame: tuple | None = mock_image_loader.get_next_frame()
+    example_frame: tuple | None = image_loader.get_next_frame()
     assert example_frame == (2, 2)
-    example_frame = mock_image_loader.get_next_frame()
+    example_frame = image_loader.get_next_frame()
     assert example_frame == (3, 3)
-    example_frame = mock_image_loader.get_next_frame()
+    example_frame = image_loader.get_next_frame()
     assert example_frame == (1, 1)
-    example_frame = mock_image_loader.get_next_frame()
+    example_frame = image_loader.get_next_frame()
 
     # if next is None, should return None but not increment internal frame index
-    mock_image_loader.aniamtion_frames[2] = None
-    example_frame = mock_image_loader.get_next_frame()
+    image_loader.aniamtion_frames[2] = None
+    example_frame = image_loader.get_next_frame()
     assert example_frame is None
-    assert mock_image_loader.frame_index == 1
+    assert image_loader.frame_index == 1
 
     # reset should set all animation variables to defaults
-    mock_image_loader.reset()
-    assert len(mock_image_loader.aniamtion_frames) == 0
-    assert mock_image_loader.frame_index == 0
+    image_loader.reset()
+    assert len(image_loader.aniamtion_frames) == 0
+    assert image_loader.frame_index == 0
