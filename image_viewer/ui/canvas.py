41427c3ba4d7ae7d62ee3132c9b86ce7a17ca8cc
jbjd <jimdemetris@yahoo.com>
2024-01-29 20:54:23 -0600

    almost fix movement

--- image_viewer/ui/canvas.py
+++ image_viewer/ui/canvas.py
@@ -1,4 +1,3 @@
-from collections.abc import Callable
 from tkinter import Canvas, Event
 
 from PIL.ImageTk import PhotoImage
@@ -7,7 +6,13 @@ from PIL.ImageTk import PhotoImage
 class CustomCanvas(Canvas):
     """Custom version of tkinter's canvas to support internal methods"""
 
-    __slots__ = "coord_outside_of_image", "image_display_id", "old_location", "topbar"
+    __slots__ = (
+        "image_display_id",
+        "old_location",
+        "screen_width",
+        "screen_height",
+        "topbar",
+    )
 
     def __init__(self, master) -> None:
         super().__init__(master, bg="black", highlightthickness=0)
@@ -23,12 +28,8 @@ class CustomCanvas(Canvas):
         self.image_display_id = self.create_image(
             screen_width >> 1, screen_height >> 1, anchor="center", tag="back"
         )
-        self.coord_outside_of_image: Callable = lambda bbox, x, y: (
-            (bbox[0] + x) + x < 0
-            or (bbox[1] + y) < 0
-            or (bbox[2] + x) > screen_width
-            or (bbox[3] + y) > screen_height
-        )
+        self.screen_width = screen_width
+        self.screen_height = screen_height
 
     def create_topbar(self, topbar_img: PhotoImage) -> None:
         """Creates the topbar and stores it"""
@@ -51,10 +52,28 @@ class CustomCanvas(Canvas):
     def handle_ctrl_arrow_keys(self, event: Event) -> None:
         """Move onscreen image when ctrl+arrow key clicked/held"""
         bbox: tuple = self.bbox(self.image_display_id)
-        # left 37, right 39, up 38, down 40
-        x, y = {37: (-10, 0), 39: (10, 0), 38: (0, -10), 40: (0, 10)}[event.keycode]
-        if self.coord_outside_of_image(bbox, x, y):
-            return
+        match event.keycode:
+            case 37:  # Left
+                x = -10
+                y = 0
+                if bbox[2] > self.screen_width or (bbox[0] + x) < 0:
+                    return
+            case 38:  # Up
+                x = 0
+                y = -10
+                if (bbox[1] + y) < 0 or bbox[3] > self.screen_height:
+                    return
+            case 39:  # Right
+                x = 10
+                y = 0
+                if (bbox[2] + x) > self.screen_width or bbox[0] < 0:
+                    return
+            case _:  # Down
+                x = 0
+                y = 10
+                if (bbox[3] + y) > self.screen_height or bbox[1] < 0:
+                    return
+
         if self.old_location is None:
             self.old_location = (bbox[0], bbox[1])
 
