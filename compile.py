6be3ecf1ae9052ef700a18a1295c60d6623cdc9b
jbjd <jimdemetris@yahoo.com>
2024-02-07 19:12:55 -0600

    break up the large compile.py file

--- compile.py
+++ compile.py
@@ -1,25 +1,12 @@
-import ast
 import importlib
 import os
 import shutil
 import subprocess
-from _ast import Name
 from argparse import REMAINDER, ArgumentParser
 from glob import glob
 from sys import version_info
 
-is_windows: bool = os.name == "nt"
-if is_windows:
-    import ctypes
-
-try:
-    import autoflake
-except ImportError:
-    print(
-        "You do not have the autoflake package installed.",
-        "Installing it will allow for a slightly smaller output\n",
-    )
-    autoflake = None
+from compile_utils.cleaner import clean_file_and_copy
 
 try:
     from nuitka import PythonVersions
@@ -45,6 +32,7 @@ INSTALL_PATH: str
 EXECUTABLE_EXT: str
 DATA_FILE_PATHS: list[str]
 
+is_windows: bool = os.name == "nt"
 if is_windows:
     INSTALL_PATH = "C:/Program Files/Personal Image Viewer/"
     EXECUTABLE_EXT = ".exe"
@@ -107,6 +95,8 @@ extra_args += (
 if not args.debug:
     is_root: bool
     if is_windows:
+        import ctypes
+
         is_root = ctypes.windll.shell32.IsUserAnAdmin() != 0
     else:
         # On windows, mypy complains
@@ -132,90 +122,6 @@ if args.python_path is None:
             )
     else:
         args.python_path = "python3"
-
-skip_repo: dict[str, tuple] = {
-    "turbojpeg": (
-        {},
-        {"crop_multiple", "scale_with_quality", "decode_to_yuv_planes"},
-    )
-}
-
-
-class TypeHintRemover(ast._Unparser):  # type: ignore
-    """Functions copied from base class, mainly edited to remove type hints"""
-
-    def __init__(self, module_name: str = "", **kwargs) -> None:
-        super().__init__(**kwargs)
-        if module_name in skip_repo:
-            self.vars_to_skip, self.func_to_skip = skip_repo[module_name]
-        else:
-            self.vars_to_skip = {}
-            self.func_to_skip = {}
-
-    def visit_FunctionDef(self, node):
-        """Removes type hints from functions"""
-        if node.name in self.func_to_skip:
-            return
-        self.maybe_newline()
-        for deco in node.decorator_list:
-            self.fill("@")
-            self.traverse(deco)
-        self.fill(f"def {node.name}")
-        if node.args.args:
-            for arg in node.args.args:
-                arg.annotation = None
-        with self.delimit("(", ")"):
-            self.traverse(node.args)
-        with self.block(extra=self.get_type_comment(node)):
-            self._write_docstring_and_traverse_body(node)
-
-    def visit_Assign(self, node):
-        """Skips over some variables"""
-        var_name: str = getattr(node.targets[0], "id", "")
-        if var_name not in self.vars_to_skip:
-            super().visit_Assign(node)
-
-    def visit_AnnAssign(self, node):
-        """Remove var annotations and declares like 'var: type' without an = after"""
-        if node.value:
-            self.fill()
-            with self.delimit_if(
-                "(", ")", not node.simple and isinstance(node.target, Name)
-            ):
-                self.traverse(node.target)
-            self.write(" = ")
-            self.traverse(node.value)
-
-    def visit_Import(self, node):
-        """Skips writing type hinting imports"""
-        if [n for n in node.names if n.name != "typing" and n.name != "collections"]:
-            super().visit_Import(node)
-
-    def visit_ImportFrom(self, node):
-        """Skips writing type hinting imports"""
-        if node.module != "typing" and node.module != "collections.abc":
-            super().visit_ImportFrom(node)
-
-
-def clean_file_and_copy(path: str, new_path: str, module_name: str = "") -> None:
-    with open(path) as fp:
-        parsed_source = ast.parse(fp.read())
-    contents: str = TypeHintRemover(module_name).visit(
-        ast.NodeTransformer().visit(parsed_source)
-    )
-
-    if autoflake:
-        contents = autoflake.fix_code(
-            contents,
-            remove_all_unused_imports=True,
-            remove_duplicate_keys=True,
-            remove_unused_variables=True,
-            remove_rhs_for_unused_variables=True,
-        )
-
-    os.makedirs(os.path.dirname(new_path), exist_ok=True)
-    with open(new_path, "w") as fp:
-        fp.write(contents)
 
 
 def move_files_to_tmp_and_clean(dir: str, mod_prefix: str = ""):
