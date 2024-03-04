c7f285f9e10d1a6c555382a27e3424c701a98be8
jbjd <jimdemetris@yahoo.com>
2024-03-03 18:38:16 -0600

    compile regex

--- image_viewer/util/os.py
+++ image_viewer/util/os.py
@@ -4,47 +4,53 @@ Code for OS specific stuff
 
 import os
 from collections.abc import Iterator
-from re import sub
+from re import Pattern, compile
 
+illegal_char: Pattern[str]
+separators: str
+kb_size: int
 if os.name == "nt":
     from ctypes import windll
 
-    illegal_char = r'[<>:"|?*]'
+    illegal_char = compile(r'[<>:"|?*]')
     separators = r"[\\/]"
-    kb_size: int = 1024
+    kb_size = 1024
 
     def OS_name_cmp(a: str, b: str) -> bool:
         return windll.shlwapi.StrCmpLogicalW(a, b) < 0
 
 else:  # linux / can't determine / unsupported OS
-    illegal_char = r"[]"
+    illegal_char = compile(r"[]")
     separators = r"[/]"
-    kb_size: int = 1000
+    kb_size = 1000
 
     def OS_name_cmp(a: str, b: str) -> bool:
         return a < b
 
 
-def get_byte_display(bytes: int) -> str:
-    size_in_kb: int = bytes // kb_size
-    return f"{size_in_kb/kb_size:.2f}mb" if size_in_kb > 999 else f"{size_in_kb}kb"
+# Setup regex for truncating path
+# shrink /./ to /
+same_dir: Pattern[str] = compile(rf"{separators}\.({separators}|$)")
+
+# shrink abc/123/../ to abc
+previous_dir: Pattern[str] = compile(
+    rf"{separators}[^{separators[1:]}+?{separators}\.\.({separators}|$)"
+)
 
 
 def truncate_path(path: str) -> str:
     """Shortens . and .. in paths"""
-    # shrink /./ to /
-    pattern: str = rf"{separators}\.({separators}|$)"
-    path = sub(pattern, "/", path)
-
-    # shrink abc/123/../ to abc
-    negated_separators: str = f"[^{separators[1:]}"  # [...] -> [^...]
-    pattern = rf"{separators}{negated_separators}+?{separators}\.\.({separators}|$)"
-    return sub(pattern, "/", path)
+    return previous_dir.sub("/", same_dir.sub("/", path))
 
 
 def clean_str_for_OS_path(file_name: str) -> str:
     """Removes characters that file names can't have on this OS"""
-    return sub(illegal_char, "", file_name)
+    return illegal_char.sub("", file_name)
+
+
+def get_byte_display(bytes: int) -> str:
+    size_in_kb: int = bytes // kb_size
+    return f"{size_in_kb/kb_size:.2f}mb" if size_in_kb > 999 else f"{size_in_kb}kb"
 
 
 def walk_dir(directory_path: str) -> Iterator[str]:
