cc59f7c7f09359dbecf1dad073927a1b4fa803a9
jbjd <jimdemetris@yahoo.com>
2024-03-06 21:08:43 -0600

    100% coverage for PIL.py

--- tests/test_image_util.py
+++ tests/test_image_util.py
@@ -4,7 +4,12 @@ from PIL import Image
 from PIL.ImageTk import PhotoImage
 
 from image_viewer.util.image import ImageName, magic_number_guess
-from image_viewer.util.PIL import create_dropdown_image, init_PIL
+from image_viewer.util.PIL import (
+    create_dropdown_image,
+    init_PIL,
+    resize,
+    get_placeholder_for_errored_image,
+)
 
 
 def test_image_path():
@@ -26,6 +31,12 @@ def test_PIL_functions(tk_app: Tk):
 
     assert isinstance(create_dropdown_image("test\ntest"), PhotoImage)
 
+    # need to test this here since init_PIL must be called first
+    example_error = Exception("test")
+    placeholder: PhotoImage = get_placeholder_for_errored_image(example_error, 10, 10)
+
+    assert type(placeholder) is PhotoImage
+
 
 def test_magic_number_guess():
     """Ensure correct image type guessed"""
@@ -37,3 +48,23 @@ def test_magic_number_guess():
 
     # When nothing else matches, guess jpeg
     assert magic_number_guess(b"ABCD")[0] == "JPEG"
+
+
+def test_resize():
+    """Test a variety of PIL Image resize scenarios"""
+
+    example_image = Image.new("P", (10, 10))
+
+    same_size_image = resize(example_image, (10, 10))
+
+    # Will not resize to the same dimensions
+    assert same_size_image is example_image
+
+    new_image = resize(example_image, (20, 20))
+
+    assert new_image.size == (20, 20)
+    assert new_image.mode == "RGB"  # P or 1 type images should convert to RGB
+
+    new_image = resize(new_image.convert("RGBA"), (15, 15))
+
+    assert new_image.size == (15, 15)
