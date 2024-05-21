a4cc96107861a8dda768907a32965d02edf16c8e
jbjd <jimdemetris@yahoo.com>
2024-05-20 20:04:49 -0500

    fix zoom

--- image_viewer/states/zoom_state.py
+++ image_viewer/states/zoom_state.py
@@ -9,21 +9,27 @@ class ZoomState:
         self.level: int = 0
         self.level_cap: int = 1024
 
-    def try_update_zoom_level(self, zoom_change: int) -> bool:
+    def try_update_zoom_level(self, zoom_change_amount: int) -> bool:
         """Updates zoom level without allowing zoom smaller than initial size
         Returns True when zoom level was updated"""
-        if zoom_change == 0:
+        if zoom_change_amount == 0:
             return False
 
         previous_level: int = self.level
-        self.level += zoom_change
+        self.level += zoom_change_amount
+        if self.level < 0:
+            self.level = 0
 
-        if zoom_change > 0:
+        zoom_did_not_changed: bool = self.level == previous_level
+        if zoom_did_not_changed:
+            return False
+
+        if zoom_change_amount > 0:
             self.zoom_scale *= self.ZOOM_DELTA
         else:
             self.zoom_scale /= self.ZOOM_DELTA
 
-        return self.level != previous_level
+        return True
 
     def reset(self) -> None:
         """Resets zoom level"""
--- tests/test_zoom_state.py
+++ tests/test_zoom_state.py
@@ -12,28 +12,14 @@ def test_try_update_zoom_level(zoom_state: ZoomState):
     """Should move zoom up or down based on input"""
     assert zoom_state.level == 0
 
-    updated = zoom_state.try_update_zoom_level(zoom_in=False)
+    updated = zoom_state.try_update_zoom_level(-1)
     assert zoom_state.level == 0
     assert not updated
 
-    updated = zoom_state.try_update_zoom_level(zoom_in=True)
+    updated = zoom_state.try_update_zoom_level(1)
     assert zoom_state.level == 1
     assert updated
 
-    updated = zoom_state.try_update_zoom_level(zoom_in=False)
+    updated = zoom_state.try_update_zoom_level(-1)
     assert zoom_state.level == 0
     assert updated
-
-
-def test_utility_functions(zoom_state: ZoomState):
-    """Ensure zoom cap sets correctly and reset goes to default values"""
-    zoom_state.level = 5
-    zoom_state.hit_cap()
-
-    assert zoom_state.cap == 5
-
-    zoom_state.reset()
-
-    default_zoom_state = ZoomState()
-    assert zoom_state.level == default_zoom_state.level
-    assert zoom_state.cap == default_zoom_state.cap
