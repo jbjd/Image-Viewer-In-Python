b220ed4f9491d6e8d5ac452b0adeea351e0af64a
jbjd <jimdemetris@yahoo.com>
2024-07-01 22:30:06 -0500

    set CFLAG -mtune=native

--- compile_utils/nuitka.py
+++ compile_utils/nuitka.py
@@ -32,7 +32,9 @@ def start_nuitka_compilation(
     print(f'Using python install "{python_path}"')
 
     compile_env = os.environ.copy()
-    compile_env["CFLAGS"] = "-O2"
+    # -march=native had a race condition that segfaulted on startup
+    # -mtune=native works as intended
+    compile_env["CFLAGS"] = "-O2 -fno-signed-zeros -mtune=native"
 
     args: list[str] = get_base_cmd_args(python_path, input_file)
     args += extra_nuitka_args
