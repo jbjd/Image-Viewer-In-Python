9817c82ae51cbc4b235ff31f9714533ada821ee5
James Demetris <63110925+jbjd@users.noreply.github.com>
2025-05-10 23:02:06 -0500

    Refactor: Clean up code in compile args module (#22)
    Fix: debug builds could hit an error in compile.py due to an improper string check

--- compile_utils/args.py
+++ compile_utils/args.py
@@ -1,8 +1,42 @@
 import os
 import sys
 from argparse import REMAINDER, ArgumentParser, Namespace
+from enum import StrEnum
+from typing import Literal
 
 from compile_utils.validation import raise_if_not_root
+
+_REPORT_NAME: str = "compilation-report.xml"
+
+
+class ConsoleMode(StrEnum):
+    """Options for console mode in nuitka"""
+
+    FORCE = "force"
+    DISABLE = "disable"
+
+
+class NuitkaArgs(StrEnum):
+    """Nuitka arguments that are used as part of compilation"""
+
+    DEPLOYMENT = "--deployment"
+    STANDALONE = "--standalone"
+    MINGW64 = "--mingw64"
+    ENABLE_PLUGIN = "--enable-plugin"
+    NO_FOLLOW_IMPORT = "--nofollow-import-to"
+    WINDOWS_ICON_FROM_ICO = "--windows-icon-from-ico"
+    WINDOWS_CONSOLE_MODE = "--windows-console-mode"
+    REMOVE_OUTPUT = "--remove-output"
+    QUIET = "--quiet"
+    VERBOSE = "--verbose"
+    REPORT = "--report"
+    WARN_IMPLICIT_EXCEPTIONS = "--warn-implicit-exceptions"
+    WARN_UNUSUAL_CODE = "--warn-unusual-code"
+    SHOW_SCONS = "--show-scons"
+    SHOW_MEMORY = "--show-memory"
+
+    def with_value(self, value: str) -> str:
+        return f"{self}={value}"
 
 
 class CompileArgumentParser(ArgumentParser):
@@ -10,14 +44,14 @@ class CompileArgumentParser(ArgumentParser):
 
     __slots__ = ()
 
-    VALID_NUITKA_ARGS: set[str] = {
-        "--standalone",
-        "--quiet",
-        "--verbose",
-        "--show-scons",
-        "--show-memory",
-        "--windows-console-mode",
-    }
+    VALID_NUITKA_ARGS: list[str] = [
+        NuitkaArgs.STANDALONE.value,
+        NuitkaArgs.QUIET.value,
+        NuitkaArgs.VERBOSE.value,
+        NuitkaArgs.SHOW_SCONS.value,
+        NuitkaArgs.SHOW_MEMORY.value,
+        NuitkaArgs.WINDOWS_CONSOLE_MODE.value,
+    ]
 
     def __init__(self, install_path: str) -> None:
         super().__init__(
@@ -25,63 +59,74 @@ class CompileArgumentParser(ArgumentParser):
             epilog=f"Some nuitka arguments are also accepted: {self.VALID_NUITKA_ARGS}",
         )
 
-        _EXPOSED_FOR_DEBUG: str = " This option is exposed for debugging"
-
         default_python: str = "python" if os.name == "nt" else "python3"
-        self.add_argument(
+        self.add_argument_ext(
             "--python-path",
-            help=(
-                "Python to use in compilation, "
-                "defaults to interpreter running this program"
-            ),
-            default=f"{sys.exec_prefix}/{default_python}",
+            "Python to use in compilation, defaults to current interpreter.",
+            f"{sys.exec_prefix}/{default_python}",
         )
-        self.add_argument(
+        self.add_argument_ext(
             "--install-path",
-            help=f"Path to install to, defaults to {install_path}",
-            default=install_path,
+            f"Path to install to, defaults to {install_path}",
+            install_path,
         )
-        self.add_argument(
+        self.add_argument_ext(
             "--report",
-            action="store_true",
-            help="Adds --report=compilation-report.xml flag to nuitka",
+            f"Adds {NuitkaArgs.REPORT.with_value(_REPORT_NAME)} flag to nuitka.",
         )
-        self.add_argument(
+        self.add_argument_ext(
             "--debug",
-            action="store_true",
-            help=(
+            (
                 "Doesn't move compiled code to install path, doesn't check for root, "
-                "doesn't cleanup, doesn't pass Go, doesn't collect $200, "
-                "adds --enable-console, --warn-implicit-exceptions, "
-                "--warn-unusual-code, --report=compilation-report.xml flags to nuitka"
+                "doesn't cleanup, doesn't pass Go, doesn't collect $200, adds "
+                f"{NuitkaArgs.WARN_IMPLICIT_EXCEPTIONS}, {NuitkaArgs.WARN_UNUSUAL_CODE}"
+                f", {NuitkaArgs.REPORT.with_value(_REPORT_NAME)}, and "
+                f"{NuitkaArgs.WINDOWS_CONSOLE_MODE}={ConsoleMode.FORCE}"
+                " flags to nuitka."
             ),
         )
-        self.add_argument(
-            "--skip-nuitka",
-            action="store_true",
-            help=(
-                "Will not compile, will only create tmp directory "
-                "with cleaned .py files." + _EXPOSED_FOR_DEBUG
-            ),
-        )
-        self.add_argument(
-            "--no-cleanup",
-            action="store_true",
-            help=(
-                "Does not delete temporary files used for build/distribution."
-                + _EXPOSED_FOR_DEBUG
-            ),
-        )
-        self.add_argument(
+        self.add_argument_ext(
             "--strip",
-            action="store_true",
-            help=(
-                "Calls strip on all .exe and .dll files after compilation. "
-                "Requires strip being installed and on PATH. "
-                "This option only works for standalone builds"
+            (
+                "Calls strip on all .exe/.dll/.pyd files after compilation. "
+                "Requires strip being installed and on PATH."
             ),
+            is_standalone_only=True,
+        )
+        self.add_argument_ext(
+            "--skip-nuitka",
+            (
+                "Skips running nuitka so no compilation takes place. "
+                "Only creates the tmp directory as it would be before compilation."
+            ),
+            is_debug=True,
+        )
+        self.add_argument_ext(
+            "--no-cleanup",
+            "Does not delete temporary files used for compilation/installation.",
+            is_debug=True,
         )
         self.add_argument("args", nargs=REMAINDER)
+
+    def add_argument_ext(
+        self,
+        name: str,
+        help: str,
+        default: Literal[False] | str = False,
+        is_debug: bool = False,
+        is_standalone_only: bool = False,
+    ) -> None:
+        """Extension of add_argument to simplify repeated patterns.
+        Help text is expanded if is_debug or is_standalone_only are True.
+        Infers if argument is store or store_true based on passed default."""
+        if is_debug:
+            help += " This option is exposed for debugging."
+        if is_standalone_only:
+            help += " This option only works for standalone builds."
+
+        action: str = "store_true" if default is False else "store"
+
+        super().add_argument(name, help=help, action=action, default=default)
 
     # for some reason mypy gets the super type wrong
     def parse_known_args(  # type: ignore
@@ -111,32 +156,34 @@ class CompileArgumentParser(ArgumentParser):
         """Given the input list of nuitka args, adds extra arguments
         based on flags user specified"""
         if user_args.report or user_args.debug:
-            nuitka_args.append("--report=compilation-report.xml")
+            nuitka_args.append(NuitkaArgs.REPORT.with_value(_REPORT_NAME))
             if user_args.debug:
-                nuitka_args += ["--warn-implicit-exceptions", "--warn-unusual-code"]
+                nuitka_args += [
+                    NuitkaArgs.WARN_IMPLICIT_EXCEPTIONS,
+                    NuitkaArgs.WARN_UNUSUAL_CODE,
+                ]
 
         if not user_args.debug:
-            nuitka_args.append("--deployment")
+            nuitka_args.append(NuitkaArgs.DEPLOYMENT)
 
             if not user_args.no_cleanup:
-                nuitka_args.append("--remove-output")
+                nuitka_args.append(NuitkaArgs.REMOVE_OUTPUT)
 
-        if "--standalone" in nuitka_args:
-            nuitka_args.append("--enable-plugin=tk-inter")
+        if NuitkaArgs.STANDALONE in nuitka_args:
+            nuitka_args.append(NuitkaArgs.ENABLE_PLUGIN.with_value("tk-inter"))
 
             nuitka_args += [
-                f"--nofollow-import-to={skipped_import}"
+                NuitkaArgs.NO_FOLLOW_IMPORT.with_value(skipped_import)
                 for skipped_import in imports_to_skip
             ]
 
-        ENABLE_CONSOLE: str = "--windows-console-mode=force"
-        DISABLE_CONSOLE: str = "--windows-console-mode=disable"
-        ATTACH_CONSOLE: str = "--windows-console-mode=attach"
-        if (
-            ENABLE_CONSOLE not in nuitka_args
-            and DISABLE_CONSOLE not in nuitka_args
-            and ATTACH_CONSOLE not in nuitka_args
+        if not any(
+            arg.startswith(NuitkaArgs.WINDOWS_CONSOLE_MODE) for arg in nuitka_args
         ):
-            nuitka_args.append(ENABLE_CONSOLE if user_args.debug else DISABLE_CONSOLE)
+            nuitka_args.append(
+                NuitkaArgs.WINDOWS_CONSOLE_MODE.with_value(
+                    ConsoleMode.FORCE if user_args.debug else ConsoleMode.DISABLE
+                )
+            )
 
         return nuitka_args
