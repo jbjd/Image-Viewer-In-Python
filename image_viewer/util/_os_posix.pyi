35bf36fd4f81eede8f487f84a64772ec7ea05780
jbjd <jimdemetris@yahoo.com>
2025-07-26 22:28:10 -0500

    Remove C additions from last branch

--- image_viewer/util/_os_posix.pyi
+++
@@ -1,5 +0,0 @@
-import os
-
-if os.name == "posix":
-    def convert_file_to_base64_and_save_to_clipboard(file: str) -> None:
-        """Reads a file, converts it to base64, and copies it to clipboard"""
