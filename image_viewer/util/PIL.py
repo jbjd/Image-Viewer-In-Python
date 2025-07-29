8ba5d6fb5fe8a1e0895b8d54ad20d9d9dd8890fc
jbjd <jimdemetris@yahoo.com>
2025-07-29 14:40:58 -0500

    PIL util test

--- image_viewer/util/PIL.py
+++ image_viewer/util/PIL.py
@@ -153,12 +153,12 @@ def _preinit() -> None:
     if _Image._initialized > 0:
         return
 
-    __import__("PIL.AvifImagePlugin", globals(), locals(), ())
-    __import__("PIL.JpegImagePlugin", globals(), locals(), ())
-    __import__("PIL.GifImagePlugin", globals(), locals(), ())
-    __import__("PIL.PngImagePlugin", globals(), locals(), ())
-    __import__("PIL.WebPImagePlugin", globals(), locals(), ())
-    __import__("PIL.DdsImagePlugin", globals(), locals(), ())
+    __import__("PIL.AvifImagePlugin", globals(), {}, ())
+    __import__("PIL.JpegImagePlugin", globals(), {}, ())
+    __import__("PIL.GifImagePlugin", globals(), {}, ())
+    __import__("PIL.PngImagePlugin", globals(), {}, ())
+    __import__("PIL.WebPImagePlugin", globals(), {}, ())
+    __import__("PIL.DdsImagePlugin", globals(), {}, ())
 
     def new_jpeg_factory(fp: IO[bytes], filename=None) -> JpegImageFile:
         return JpegImageFile(fp, filename)
